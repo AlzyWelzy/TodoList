f7b2a13dd591cf535d3f94c37623129070ce2bd3
AlzyWelzy <welzyalzy@gmail.com>
2023-05-06 09:11:41 +0530

    changed a bit of code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class TodoList:
 
     def print(self):
         for i, todo in enumerate(self.todos):
-            print(f"{i}: {todo}")
+            print(f"{i+1}: {todo}")
 
     def save(self):
         path = Path("todos.txt")
@@ -40,7 +40,7 @@ class TodoList:
             elif command == "remove":
                 self.print()
                 index = int(input("What index would you like to remove? "))
-                self.remove(self.todos[index])
+                self.remove(self.todos[index-1])
             elif command == "print":
                 self.print()
             elif command == "save":
