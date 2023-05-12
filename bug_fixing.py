9c6f4d91c20ecd1b5d47c4f98936e58c88310322
AlzyWelzy <welzyalzy@gmail.com>
2023-05-12 09:59:30 +0530

    did the bug exercise

---
+++ bug_fixing.py
@@ -0,0 +1,9 @@
+temperatures = [10, 12, 14]
+
+file = open("file.txt", "w")
+
+file.writelines(str(temperatures))
+
+numbers = [10.1, 12.3, 14.7]
+numbers = [int(number) for number in numbers]
+print(numbers)
