a0397566cd27a421bf3ea57f3796eeb1bf6118dd
AlzyWelzy <welzyalzy@gmail.com>
2023-05-13 15:39:25 +0530

    doing the bug fixing

--- bug_fixing.py
+++ bug_fixing.py
@@ -7,3 +7,12 @@ file.writelines(str(temperatures))
 numbers = [10.1, 12.3, 14.7]
 numbers = [int(number) for number in numbers]
 print(numbers)
+
+with open("dump_file.txt", "w") as file:
+    file.writelines("Hello You!")
+
+with open("dump_file.txt", "r") as file:
+    content = file.read()
+    print(content)
+    print(type(content))
+    print(len(content))
