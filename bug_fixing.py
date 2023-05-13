2bad1ccbc9fda6a42f91dab892d53abb1a8e1e00
AlzyWelzy <welzyalzy@gmail.com>
2023-05-13 16:37:30 +0530

    added more code

--- bug_fixing.py
+++ bug_fixing.py
@@ -9,10 +9,22 @@ numbers = [int(number) for number in numbers]
 print(numbers)
 
 with open("dump_file.txt", "w") as file:
-    file.writelines("Hello You!")
+    file.writelines("Hello You!" + "\n")
+    file.writelines("Hello You!" + "\n")
+    file.writelines("Hello You!" + "\n")
+    file.writelines("Hello You!" + "\n")
+    file.writelines("Hello You!" + "\n")
+
 
 with open("dump_file.txt", "r") as file:
     content = file.read()
+    print(content)
+    print(type(content))
+    print(len(content))
+
+with open("dump_file.txt", "r") as file:
+    content = file.readlines()
+    content = [line.strip() for line in content]
     print(content)
     print(type(content))
     print(len(content))
