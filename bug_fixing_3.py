3306139dbc16b83f6f89c1eed507e76b3b6307df
AlzyWelzy <welzyalzy@gmail.com>
2023-05-20 20:11:06 +0530

    made it even better

--- bug_fixing_3.py
+++ bug_fixing_3.py
@@ -2,9 +2,9 @@ def speed(distance, time):
     return distance / time
 
 
-print(speed([200, 4]))
+# print(speed([200, 4]))
 print(speed(*[200, 4]))
 
-print(speed([5, 300]))
-print(speed([300, 5]))
+# print(speed([5, 300]))
+# print(speed([300, 5]))
 print(speed(*[300, 5]))
