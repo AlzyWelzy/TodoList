47bb4c083335491828e5c07cd31d56fa91330669
AlzyWelzy <welzyalzy@gmail.com>
2023-05-24 08:54:45 +0530

    this looks better

--- e2.py
+++ e2.py
@@ -2,8 +2,20 @@ import csv
 
 with open("some_stuff.csv", "r") as f:
     reader = csv.reader(f)
-    for row in reader:
-        print(row)
+    # for row in reader:
+    #     print(row)
 
-    # reader_list = list(reader)
-    # print(reader_list)
+    reader_list = list(reader)
+    print(reader_list)
+city = input("Enter a city: ").lower()
+
+for cities in reader_list:
+    if city in cities:
+        # print(cities)
+        # print(f"Found {city} in {cities}")
+        print(f"{city.capitalize()} has {cities[1]} million people")
+        break
+    else:
+        continue
+else:
+    print("Not found")
