b9f77a6e5dabd260d991167c709f5bea16586e7c
AlzyWelzy <welzyalzy@gmail.com>
2023-05-25 19:11:54 +0530

    doing something

--- bonus.py
+++ bonus.py
@@ -10,10 +10,14 @@ data = json.loads(content)
 # print(questions)
 
 print(data)
-
+score = 0
 for question in data:
     print(question["question_text"])
     for index, alternative in enumerate(question["alternatives"]):
         print(f"{index+1}: {alternative}")
     user_choice = int(input("Enter your choice: "))
     question["user_choice"] = user_choice
+    if question["user_choice"] == question["correct_answer"]:
+        score += 1
+
+print(f"Your score is {score} out of {len(data)}")
