aa18c28488d996d52473d625e8245e26ed758102
AlzyWelzy <welzyalzy@gmail.com>
2023-05-27 13:03:16 +0530

    adding code

--- gui.py
+++ gui.py
@@ -1,5 +1,16 @@
 import functions
-import PySimpleGUI
+import PySimpleGUI as sg
 
+label = sg.Text("Enter a to-do item")
+input_box = sg.InputText(tooltip="Enter a task")
+add_button = sg.Button("Add")
 
-PySimpleGUI.Window("My To-Do App")
+window = sg.Window(
+    "My To-Do App",
+    layout=[
+        [label],
+        [input_box, add_button],
+    ],
+)
+window.read()
+window.close()
