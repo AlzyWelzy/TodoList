fdfe88d4d8c97bea567db9f5aad03250f94a1234
AlzyWelzy <welzyalzy@gmail.com>
2023-05-28 22:54:53 +0530

    it's finally working

--- stuff/zip_creator.py
+++ stuff/zip_creator.py
@@ -5,9 +5,9 @@ import pathlib
 def make_archive(filepaths, dest_dir):
     dest_path = pathlib.Path(dest_dir, "compressed.zip")
     with zipfile.ZipFile(dest_path, "w") as zip:
-        file = pathlib.Path(file)
         for file in filepaths:
-            zip.write(file, arcname=file.name)
+            file = pathlib.Path(file)
+            zip.write(filename=file, arcname=file.name)
 
 
 if __name__ == "__main__":
