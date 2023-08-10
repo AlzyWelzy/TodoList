d856fbf4f0055cfe3f90007bae1c3debb22ce0bd
AlzyWelzy <welzyalzy@gmail.com>
2023-08-10 09:59:53 +0530

    day_1_bug_3 fixed

---
+++ day_1/bug_fixing/exercise_3.py
@@ -0,0 +1,6 @@
+# my_answer = input(What is your answer?)
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+my_answer = input("What is your answer?")
+answers = ["Yes", "No", "Yes", "No", my_answer]
+print(answers)
