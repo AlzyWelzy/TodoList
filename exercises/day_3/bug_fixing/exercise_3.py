d41954d2f8475be869c8f048e603d2082351af93
AlzyWelzy <welzyalzy@gmail.com>
2023-08-16 23:03:24 +0530

    remove

--- exercises/day_3/bug_fixing/exercise_3.py
+++ exercises/day_3/bug_fixing/exercise_3.py
@@ -1,6 +1,5 @@
 # for item in ["sandals", "glasses", "trousers"):
 #     print(item.capitalize)
-#     print(item.capitalize)
 
 for item in ["sandals", "glasses", "trousers"]:
     print(item.capitalize())
