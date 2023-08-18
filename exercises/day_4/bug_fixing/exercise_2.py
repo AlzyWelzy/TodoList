a8a3babb81118a936d890fa72d59f9152e373e0a
AlzyWelzy <welzyalzy@gmail.com>
2023-08-18 12:38:40 +0530

    create a new and more begineer friendly way

--- exercises/day_4/bug_fixing/exercise_2.py
+++ exercises/day_4/bug_fixing/exercise_2.py
@@ -5,5 +5,14 @@
 
 elements = ["a", "b", "c"]
 print(elements)
-elements = [i.replace("b", "x") for i in elements]
-print(elements)
+# elements = [i.replace("b", "x") for i in elements]
+# print(elements)
+
+new_elements = []
+for element in elements:
+    if element == "b":
+        new_elements.append("x")
+        continue
+    new_elements.append(element)
+
+print(new_elements)
