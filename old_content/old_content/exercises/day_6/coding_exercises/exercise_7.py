f0561b1dcaa11daa7088e38e5d486fb723f8b653
AlzyWelzy <welzyalzy@gmail.com>
2023-08-21 22:45:30 +0530

    done coding exercies day 6

---
+++ old_content/old_content/exercises/day_6/coding_exercises/exercise_7.py
@@ -0,0 +1,13 @@
+with open("./a.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
+
+with open("./b.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
+
+with open("./c.txt", "r") as file:
+    for line in file:
+        print(line.strip())
