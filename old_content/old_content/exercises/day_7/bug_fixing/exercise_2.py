16b733e4a41d1aad62bcd426267ad27992d9f573
AlzyWelzy <welzyalzy@gmail.com>
2023-08-22 10:34:56 +0530

    did it as well

---
+++ old_content/old_content/exercises/day_7/bug_fixing/exercise_2.py
@@ -0,0 +1,8 @@
+# numbers = [10.1, 12.3, 14.7]
+# numbers = [int(number) for item in numbers]
+# print(numbers)
+
+
+numbers = [10.1, 12.3, 14.7]
+numbers = [int(item) for item in numbers]
+print(numbers)
