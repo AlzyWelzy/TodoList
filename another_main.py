f78d66408858cfd37311456a86c6aa43c9d6681b
AlzyWelzy <welzyalzy@gmail.com>
2023-08-24 05:08:23 +0530

    helping friends

---
+++ another_main.py
@@ -0,0 +1,4 @@
+filename = 215771007006
+
+with open(f"{filename}.txt", "r") as file:
+    print(file.read())
--- main.py
+++ main.py
@@ -1,41 +1,49 @@
-def save_biodata(filename, data):
-    with open(filename, "w") as file:
-        for key, value in data.items():
-            print(f"{key}: {value}", file=file)
+# Get the user's ID number
+id_number = input("Enter your ID number: ")
+
+
+# Function to get user input and format as <item name>: value
+def get_input(item_name):
+    return input(f"Enter your {item_name}: ")
 
 
 # Collect user inputs
-data = {}
-data["Name"] = input("Enter your Name: ")
-data["Civil Status"] = input("Enter your Civil Status: ")
-data["Gender"] = input("Enter your Gender: ")
-data["Birthdate"] = input("Enter your Birthdate: ")
-data["Age"] = input("Enter your Age: ")
-data["Birthplace"] = input("Enter your Birthplace: ")
-data["Birth Country"] = input("Enter your Birth Country: ")
-data["Street, Barangay"] = input("Enter your Street, Barangay: ")
-data["Municipality, Province"] = input("Enter your Municipality, Province: ")
-data["Mobile Number"] = input("Enter your Mobile Number: ")
-data["Email Address"] = input("Enter your Email Address: ")
-data["Nationality"] = input("Enter your Nationality: ")
-data["Is Working-Student?"] = input("Enter if you are a Working Student (Yes/No): ")
-data["Course"] = input("Enter your Course: ")
-data["Name of the last school attended"] = input(
-    "Enter the Name of the last school attended: "
-)
-data["Guardian's Name"] = input("Enter your Guardian's Name: ")
-data["Are you a new student?"] = input("Are you a new student? (Yes/No): ")
-data["Are you a shifter?"] = input("Are you a shifter? (Yes/No): ")
-data["Are you a transferee?"] = input("Are you a transferee? (Yes/No): ")
-data["Do you love programming so far?"] = input(
-    "Do you love programming so far? (Yes/No): "
-)
+biodata = [
+    "Name",
+    "Civil Status",
+    "Gender",
+    "Birthdate",
+    "Age",
+    "Birthplace",
+    "Birth Country",
+    "Street, Barangay",
+    "Municipality, Province",
+    "Mobile Number",
+    "Email Address",
+    "Nationality",
+    "Is Working-Student?",
+    "Course",
+    "Name of the last school attended",
+    "Guardian's Name",
+    "Are you a new student?",
+    "Are you a shifter?",
+    "Are you a transferee?",
+    "Do you love programming so far?",
+]
 
-# Generate a unique ID number based on the Name and Birthdate (you can replace this with an actual algorithm)
-id_number = data["Name"].split()[0].lower() + data["Birthdate"].replace("-", "")
+# Format and store biodata
+formatted_biodata = ""
+for item in biodata:
+    user_input = get_input(item)
+    formatted_biodata += f"{item}: {user_input}\n"
 
-# Save data to a text file with the unique ID number as the filename
-filename = f"{id_number}.txt"
-save_biodata(filename, data)
+# Save biodata to a file
+file_name = f"{id_number}.txt"
+with open(file_name, "w") as file:
+    file.write(formatted_biodata)
 
 print("Biodata saved successfully.")
+
+
+with open(file_name, "r") as file:
+    print(file.read())
