4562001be97a4a2bbd0382b92e1d4a452f5d55cc
AlzyWelzy <welzyalzy@gmail.com>
2023-08-24 20:24:58 +0530

    add

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 # Full names of block mates
-full_names = ["John Doe", "Jane Smith", "Michael Johnson", "Emily Brown", "David Wilson"]
+full_names = [
+    "John Doe",
+    "Jane Smith",
+    "Michael Johnson",
+    "Emily Brown",
+    "David Wilson",
+]
 
 # Nicknames of block mates
-nicknames = ["JD", "JS", "MJ", "EB", "DW"]
+nicknames = [
+    "JD",
+    "JS",
+    "MJ",
+    "EB",
+    "DW",
+]
 
 # Display full names
-print(*full_names, sep='\\', end='.')
+print(*full_names, sep="\\", end=".")
+
+# divider
+print()
+
 
 # Display nicknames
-print(*nicknames, sep='\t', end='\n')
+print(*nicknames, sep="\t", end="\n")
 
 # Display end message
 print("End of task number 4.")
