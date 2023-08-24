b12a3a1611909815511341f22f54ecb61d235c97
AlzyWelzy <welzyalzy@gmail.com>
2023-08-24 19:08:43 +0530

    new assignment

--- main.py
+++ main.py
@@ -1,49 +1,14 @@
-# Get the user's ID number
-id_number = input("Enter your ID number: ")
+# Full names of block mates
+full_names = ["John Doe", "Jane Smith", "Michael Johnson", "Emily Brown", "David Wilson"]
 
+# Nicknames of block mates
+nicknames = ["JD", "JS", "MJ", "EB", "DW"]
 
-# Function to get user input and format as <item name>: value
-def get_input(item_name):
-    return input(f"Enter your {item_name}: ")
+# Display full names
+print(*full_names, sep='\\', end='.')
 
+# Display nicknames
+print(*nicknames, sep='\t', end='\n')
 
-# Collect user inputs
-biodata = [
-    "Name",
-    "Civil Status",
-    "Gender",
-    "Birthdate",
-    "Age",
-    "Birthplace",
-    "Birth Country",
-    "Street, Barangay",
-    "Municipality, Province",
-    "Mobile Number",
-    "Email Address",
-    "Nationality",
-    "Is Working-Student?",
-    "Course",
-    "Name of the last school attended",
-    "Guardian's Name",
-    "Are you a new student?",
-    "Are you a shifter?",
-    "Are you a transferee?",
-    "Do you love programming so far?",
-]
-
-# Format and store biodata
-formatted_biodata = ""
-for item in biodata:
-    user_input = get_input(item)
-    formatted_biodata += f"{item}: {user_input}\n"
-
-# Save biodata to a file
-file_name = f"{id_number}.txt"
-with open(file_name, "w") as file:
-    file.write(formatted_biodata)
-
-print("Biodata saved successfully.")
-
-
-with open(file_name, "r") as file:
-    print(file.read())
+# Display end message
+print("End of task number 4.")
