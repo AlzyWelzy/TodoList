d43b98e5f27bbdaa604d3fb8d9d90ac9164001f8
AlzyWelzy <welzyalzy@gmail.com>
2023-08-24 07:13:04 +0530

    fixing

---
+++ old_content/old_content/exercises/day_8/bug_fixing/exercise_1.py
@@ -0,0 +1,8 @@
+# with open("file.txt", 'r') as file:
+#     print(file.read())
+#     print(len(file.read()))
+
+
+with open("file.txt", "r") as file:
+    print(file.read())
+    print(len(file.read()))
