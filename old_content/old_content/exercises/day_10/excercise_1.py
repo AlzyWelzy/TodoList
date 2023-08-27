babfd5eb20f9b09a07b6983c2a5ce03aa79025d8
AlzyWelzy <welzyalzy@gmail.com>
2023-08-27 10:22:33 +0530

    done

--- old_content/old_content/exercises/day_10/excercise_1.py
+++ old_content/old_content/exercises/day_10/excercise_1.py
@@ -3,3 +3,12 @@
 
 # number = waiting_list.index(name)
 # print(f"{name}'s turn is {number}")
+
+waiting_list = ["john", "marry"]
+name = input("Enter name: ")
+try:
+    number = waiting_list.index(name)
+    print(f"{name}'s turn is {number}")
+
+except ValueError:
+    print(f"{name} not in waiting list")
