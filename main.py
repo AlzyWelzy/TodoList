a53db9776b9a4907e29fb631dcd34dd0267ca650
AlzyWelzy <welzyalzy@gmail.com>
2023-09-13 20:57:14 +0530

    made lines more sarcastic

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def add_todo():
     st.session_state.new_todo = ""
 
 
-st.title("My Todo App")
-st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.title("Todo App")
+st.subheader("What are you waiting for?")
+st.write("How about you get your work done?")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(
